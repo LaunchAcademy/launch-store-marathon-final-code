9ea0acff6be4c345111750507e7282f035d0db50
Brianna Kincart <briannakincart@gmail.com>
2021-03-25 11:33:10 -0700

    final code A5 BK group

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,9 +1,37 @@
-import fs from 'fs'
+import fs, { readFileSync } from 'fs'
 
 const productsPath = "products.json"
 
 class Product {
+  constructor({name, price, description, available}) {
+    this.name = name
+    this.price = price
+    this.description = description
+    this.available = available
+  }
 
+  static findAll() {
+    // read JSON file and parse into object
+    const productsData = JSON.parse(fs.readFileSync(productsPath)).products
+    // take the array of objects and map into Product objects
+    let products = productsData.map(product => {
+      let newProduct = new Product(product)
+      return newProduct
+    })
+    // return array of objects
+    return products
+  }
+
+  save() {
+    // get products array
+    const products = this.constructor.findAll()
+    //avaible true
+    this.available = true
+    // save this to array
+    products.push(this)
+    //write the new array to file
+    fs.writeFileSync(productsPath, JSON.stringify({ products: products}) )
+  }
 }
 
 export default Product
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -4,4 +4,33 @@ import Product from "../models/Product.js"
 
 const productsRouter = express.Router()
 
+productsRouter.get("/", (req, res) => {
+  res.render('products/index', { products: Product.findAll() })
+})
+
+productsRouter.get("/new", (req, res) => {
+  res.render("products/new")
+})
+
+productsRouter.post("/", (req, res) => {
+  const userName = req.body.name
+  const userDescription = req.body.description
+  const userPrice = parseFloat(req.body.price)
+  if(userName && userPrice){
+    const newProduct = new Product({
+      name: userName,
+      description: userDescription,
+      price: userPrice
+    })
+    newProduct.save()
+    // redirect expects the URL path -- we add a leading slash
+    res.redirect("/products")
+  }
+  else {
+    // render expects the file for the view
+    // we DON'T give it a leading slash, it knows to look in the `views` folder
+    res.render("products/new")
+  }
+})
+
 export default productsRouter
