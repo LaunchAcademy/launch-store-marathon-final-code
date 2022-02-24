7a8f5b30185e4029493a18d768966ec600c17547
Stephen Fang <safang99@gmail.com>
2022-02-24 14:29:42 -0500

    final code

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,9 +1,47 @@
-import fs from 'fs'
+import fs from "fs"
 
 const productsPath = "products.json"
 
 class Product {
+  constructor({ name, description, price, available }) {
+    this.name = name
+    this.description = description
+    this.price = price
+    this.available = available
+  }
 
+  static findAll() {
+    const stringifiedData = fs.readFileSync(productsPath)
+    const productData = JSON.parse(stringifiedData).products
+
+    const productsList = productData.map((product) => {
+      const newProduct = new Product(product)
+      return newProduct
+    })
+
+    return productsList
+  }
+
+  //with check in router
+  // save() {
+  //   const productsList = Product.findAll()
+  //   this.available = true
+  //   productsList.push(this)
+  //   fs.writeFileSync(productsPath, JSON.stringify({ products: productsList }))
+  // }
+
+  // to return true or false
+  save() {
+    if (this.name && this.description && this.price) {
+      const productsList = Product.findAll()
+      this.available = true
+      productsList.push(this)
+      fs.writeFileSync(productsPath, JSON.stringify({ products: productsList }))
+      return true
+    } else {
+      return false
+    }
+  }
 }
 
 export default Product
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -4,4 +4,39 @@ import Product from "../models/Product.js"
 
 const productsRouter = express.Router()
 
+productsRouter.get("/", (req, res) => {
+  const products = Product.findAll()
+  res.render("products/index", { products: products })
+})
+
+productsRouter.get("/new", (req, res) => {
+  res.render("products/new")
+})
+
+productsRouter.post("/", (req, res) => {
+  const formData = req.body
+  const newProduct = new Product(formData)
+
+  console.log(newProduct)
+
+  // with conditional check for formdata here
+  // if (
+  //   req.body.name.trim() === "" ||
+  //   req.body.description.trim() === "" ||
+  //   req.body.price.trim() === ""
+  // ) {
+  //   res.render("products/new")
+  // } else {
+  //   newProduct.save()
+  //   res.redirect("/products")
+  // }
+
+  // // With .save returning true/false
+  if (newProduct.save()) {
+    res.redirect("/products")
+  } else {
+    res.render("products/new")
+  }
+})
+
 export default productsRouter
